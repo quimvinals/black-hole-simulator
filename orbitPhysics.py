bd3306db160f654157f3d08eadbf5e23b6e55060
BalisticMan <quim.baget@estudiantat.upc.edu>
2021-10-17 15:54:10 +0200

    fixed

--- orbitPhysics.py
+++ orbitPhysics.py
@@ -1,9 +1,11 @@
 import numpy as PI
 
+"""OrbitPhysics class"""
+"""This class implements the necessary methods to calculate orbits """
 class OrbitPhysics:
     G = 4 * PI.pi**2
 
-    def euler_method(x0, y0, vx0, vy0, relativeHoleMass, relativeStarMass, xPositions, yPositions):
+    def euler_method(self, x0, y0, vx0, vy0, relativeHoleMass, relativeStarMass, xPositions, yPositions):
         orbitEnergy = PI.array([])
         dt = .001
         # Initialize velocities
@@ -16,8 +18,8 @@ class OrbitPhysics:
 
         for i in PI.linspace(0,1,3001): # Itera 100 vegades
             r = PI.sqrt(x**2+y**2)
-            ax = -G*relativeHoleMass*x/r**3
-            ay = -G*relativeHoleMass*y/r**3
+            ax = - self.G * relativeHoleMass*x/r**3
+            ay = - self.G * relativeHoleMass*y/r**3
             #calc next position and next vel
             x = x + vx*dt
             y = y + vy*dt
@@ -25,5 +27,5 @@ class OrbitPhysics:
             vy = vy + ay*dt
             xPositions.append(x)
             yPositions.append(y)
-            orbitEnergy = PI.append(orbitEnergy,.5*relativeStarMass*(vx**2+vy**2)-G*relativeStarMass*relativeHoleMass/r)
+            orbitEnergy = PI.append(orbitEnergy,.5*relativeStarMass*(vx**2+vy**2)-self.G*relativeStarMass*relativeHoleMass/r)
         return orbitEnergy
